f9a53d6f0b5ad30897603ceaf3e1a9531fbc341d
fan-k-manabe <k_manabe@fancs.com>
2015-12-07 11:07:47 +0900

    Add unused xib sample.

---
+++ Sample/NendSDK_Sample/NativeAdNoNibViewController.h
@@ -0,0 +1,12 @@
+//
+//  NativeAdNoNibViewController.h
+//  NendSDK_Sample
+//
+//  Copyright © 2015年 F@N Communications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NativeAdNoNibViewController : UIViewController
+
+@end
---
+++ Sample/NendSDK_Sample/NativeAdNoNibViewController.m
@@ -0,0 +1,92 @@
+//
+//  NativeAdNoNibViewController.m
+//  NendSDK_Sample
+//
+//  Copyright © 2015年 F@N Communications. All rights reserved.
+//
+
+#import "NativeAdNoNibViewController.h"
+
+#import "NADNativeClient.h"
+
+@interface NativeAdViewInternal : UIView <NADNativeViewRendering>
+
+@property (nonatomic, strong) NADNativeImageView *nativeAdImageView;
+@property (nonatomic, strong) NADNativeLabel *nativeAdPrTextLabel;
+@property (nonatomic, strong) NADNativeLabel *nativeAdShortTextLabel;
+
+@end
+
+@interface NativeAdNoNibViewController ()
+
+@property (nonatomic, strong) NADNativeClient *client;
+
+@end
+
+@implementation NativeAdNoNibViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.client = [[NADNativeClient alloc] initWithSpotId:@"485500"
+                                                   apiKey:@"10d9088b5bd36cf43b295b0774e5dcf7d20a4071"
+                                    advertisingExplicitly:NADNativeAdvertisingExplicitlyPR];
+    
+    UIView<NADNativeViewRendering> *adView = [[NativeAdViewInternal alloc] initWithFrame:CGRectMake(0.f, 0.f, 320.f, 96.f)];
+    adView.center = CGPointMake(CGRectGetMidX(self.view.frame), CGRectGetMidY(self.view.frame));
+    [self.view addSubview:adView];
+    [self.client loadWithCompletionBlock:^(NADNative *ad, NSError *error) {
+        if (ad) {
+            [ad intoView:adView];
+        }
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
+
+@implementation NativeAdViewInternal
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _nativeAdImageView = [[NADNativeImageView alloc] initWithFrame:CGRectMake(8.f, 8.f, 80.f, 80.f)];
+        _nativeAdShortTextLabel = [[NADNativeLabel alloc] initWithFrame:CGRectMake(96.f, 8.f, 218.f, 40.f)];
+        _nativeAdShortTextLabel.adjustsFontSizeToFitWidth = YES;
+        _nativeAdShortTextLabel.minimumScaleFactor = 0.5f;
+        _nativeAdPrTextLabel = [[NADNativeLabel alloc] initWithFrame:CGRectMake(96.f, 48.f, 218.f, 40.f)];
+        [self addSubview:_nativeAdImageView];
+        [self addSubview:_nativeAdShortTextLabel];
+        [self addSubview:_nativeAdPrTextLabel];
+        
+        self.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        self.layer.borderWidth = 1.f;
+    }
+    return self;
+}
+
+#pragma mark - NADNativeViewRendering
+
+- (NADNativeLabel *)prTextLabel
+{
+    return self.nativeAdPrTextLabel;
+}
+
+- (NADNativeLabel *)shortTextLabel
+{
+    return self.nativeAdShortTextLabel;
+}
+
+- (NADNativeImageView *)adImageView
+{
+    return self.nativeAdImageView;
+}
+
+
+@end

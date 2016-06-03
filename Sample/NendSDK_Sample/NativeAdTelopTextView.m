1871d28a332a51332b28e355de2f1dba63c9e500
c_yu <c_yu@fancs.com>
2016-06-03 12:09:13 +0900

    テロップ風テキスト表示

---
+++ Sample/NendSDK_Sample/NativeAdTelopTextView.m
@@ -0,0 +1,98 @@
+//
+//  NativeAdTelopShortTextView.m
+//  NendSDK_Sample
+//
+//  Copyright © 2016年 F@N Communications. All rights reserved.
+//
+
+#import "NativeAdTelopTextView.h"
+#import "NADNativeViewRendering.h"
+#define timerInterval     0.02f
+#define distance          1.f
+
+@interface NativeAdTelopTextView ()<NADNativeViewRendering>
+
+@property (nonatomic, weak) IBOutlet NADNativeImageView *nativeAdImageView;
+@property (nonatomic, weak) IBOutlet NADNativeLabel *nativeAdPrTextLabel;
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) IBOutlet NADNativeLabel *nativeAdShortTextLabel;
+
+@property (nonatomic) float width;
+@property (nonatomic) float point;
+@property (nonatomic) NSTimer *timer;
+
+@end
+
+@implementation NativeAdTelopTextView
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    _nativeAdPrTextLabel.adjustsFontSizeToFitWidth = YES;
+    _nativeAdPrTextLabel.minimumScaleFactor = 0.5f;
+    
+    _scrollView.clipsToBounds = YES;
+    _scrollView.scrollEnabled = NO;
+    
+    self.clipsToBounds = YES;
+    self.layer.borderWidth = 1.f;
+}
+
+- (void) layoutSubviews {
+    [super layoutSubviews];
+}
+
+#pragma mark - NADNativeViewRendering
+
+- (NADNativeImageView *)adImageView
+{
+    return self.nativeAdImageView;
+}
+
+- (NADNativeLabel *)prTextLabel
+{
+    return self.nativeAdPrTextLabel;
+}
+
+- (NADNativeLabel *)shortTextLabel
+{
+    return self.nativeAdShortTextLabel;
+}
+
+- (void)startTelop {
+    
+    _width = [_nativeAdShortTextLabel.text sizeWithAttributes:@{NSFontAttributeName:_nativeAdShortTextLabel.font}].width;
+    
+    _point = -_scrollView.frame.size.width;
+    [self changeFrame];
+    
+    [self stopTimer];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:timerInterval
+                                     target:self
+                                   selector:@selector(animation)
+                                   userInfo:nil
+                                    repeats:YES];
+}
+
+- (void)animation {
+    if (_point >= _width) {
+        _point = -_scrollView.frame.size.width;
+    } else {
+        _point += distance;
+    }
+    
+    [self changeFrame];
+}
+
+- (void)changeFrame {
+    [_scrollView setContentOffset:CGPointMake(_point, 0)];
+}
+
+- (void)stopTimer {
+    if ([_timer isValid]) {
+        [_timer invalidate];
+    }
+}
+
+@end

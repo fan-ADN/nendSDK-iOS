c00c30e193d8676a4f04eaca60c56637a30c1efc
fan-t-kinami <t_kinami@fancs.com>
2018-12-19 13:25:15 +0900

    Update using isMuteStartPlaying.

--- Sample/NendSDK_Swift_Sample/VideoViewController.swift
+++ Sample/NendSDK_Swift_Sample/VideoViewController.swift
@@ -21,6 +21,7 @@ class VideoViewController: UIViewController {
         self.rewardedVideo.delegate = self
         
         self.interstitialVideo.userId = "user id"
+        self.interstitialVideo.isMuteStartPlaying = false
         self.interstitialVideo.delegate = self
         self.interstitialVideo.addFallbackFullboard(withSpotId: "485504", apiKey: "30fda4b3386e793a14b27bedb4dcd29f03d638e5")
     }

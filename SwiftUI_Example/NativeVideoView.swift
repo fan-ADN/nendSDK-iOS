6347f5f9c2fd4f4f7373b9be620f85e3320beb73
funakoshi-dev <70654005+funakoshi-dev@users.noreply.github.com>
2022-02-01 17:19:41 +0900

    [WIP] In progress

--- SwiftUI_Example/NativeVideoView.swift
+++ SwiftUI_Example/NativeVideoView.swift
@@ -2,14 +2,57 @@
 //  NativeVideoView.swift
 //  SwiftUI_Example
 //
-//  Copyright © 2021 FAN Communications. All rights reserved.
+//  Copyright © 2022 FAN Communications. All rights reserved.
 //
 
 import SwiftUI
+import NendAd
 
 struct NativeVideoView: View {
+    
+    let nativeVideoDelegate = NativeVideoDelegate()
+    let videoAdLoader = NADNativeVideoLoader(spotID: 887595, apiKey: "e7c1e68e7c16e94270bf39719b60534596b1e70d", clickAction: .fullScreen)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Group{
+                Spacer()
+                Text("Native")
+                Spacer()
+                //Showボタン
+                Button(action: {
+                    ShowTapped()
+                }) {
+                    Text("Show")
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    func ShowTapped(){
+        videoAdLoader.loadAd { (ad, error) in
+            if let videoAd = ad {
+                // success
+                videoAd.delegate = nativeVideoDelegate
+            } else {
+                // failure
+            }
+        }
+    }
+}
+
+class NativeVideoDelegate: NSObject, NADNativeVideoDelegate{
+    func nadNativeVideoDidImpression(_ ad: NADNativeVideo) {
+        print("\(#function)")
+    }
+    
+    func nadNativeVideoDidClickAd(_ ad: NADNativeVideo) {
+        print("\(#function)")
+    }
+    
+    func nadNativeVideoDidClickInformation(_ ad: NADNativeVideo) {
+        print("\(#function)")
     }
 }
 

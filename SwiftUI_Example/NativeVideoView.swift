8576ee60ed5862cfd4726135aaab9a0e41ed7def
funakoshi-dev <70654005+funakoshi-dev@users.noreply.github.com>
2022-02-02 13:28:20 +0900

    Implemented Load method and display Text

--- SwiftUI_Example/NativeVideoView.swift
+++ SwiftUI_Example/NativeVideoView.swift
@@ -10,18 +10,23 @@ import NendAd
 
 struct NativeVideoView: View {
     
-    let nativeVideoDelegate = NativeVideoDelegate()
-    let videoAdLoader = NADNativeVideoLoader(spotID: 887595, apiKey: "e7c1e68e7c16e94270bf39719b60534596b1e70d", clickAction: .fullScreen)
+    @ObservedObject var viewModel: ExternalModel = ExternalModel()
     
     var body: some View {
         VStack {
             Group{
                 Spacer()
-                Text("Native")
+                Text(self.viewModel.title)
+                    .frame(maxWidth: .infinity,maxHeight:50,alignment: .leading)
+                Text(self.viewModel.explanation)
+                    .frame(maxWidth: .infinity,maxHeight:50,alignment: .leading)
+                Text(self.viewModel.advertiserName)
+                    .frame(maxWidth: .infinity,maxHeight:50,alignment: .leading)
+                //                Image(self.viewModel.logo)
                 Spacer()
                 //Showボタン
                 Button(action: {
-                    ShowTapped()
+                    self.viewModel.LoadAd()
                 }) {
                     Text("Show")
                 }
@@ -29,12 +34,24 @@ struct NativeVideoView: View {
             }
         }
     }
+}
+
+class ExternalModel: ObservableObject{
+    @Published var title:String = ""
+    @Published var explanation:String = ""
+    @Published var advertiserName:String = ""
+    //    @Published var logo:Image
+    let nativeVideoDelegate = NativeVideoDelegate()
+    let videoAdLoader = NADNativeVideoLoader(spotID: 887595, apiKey: "e7c1e68e7c16e94270bf39719b60534596b1e70d", clickAction: .fullScreen)
     
-    func ShowTapped(){
+    func LoadAd(){
         videoAdLoader.loadAd { (ad, error) in
             if let videoAd = ad {
                 // success
-                videoAd.delegate = nativeVideoDelegate
+                videoAd.delegate = self.nativeVideoDelegate
+                self.title = "Title : " + videoAd.title!
+                self.explanation = "Explanation : " + videoAd.explanation!
+                self.advertiserName = "AdvertiserName : " + videoAd.advertiserName!
             } else {
                 // failure
             }

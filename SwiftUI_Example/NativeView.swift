df01eb122c9257346c880570235d34eadd5aff2a
funakoshi-dev <70654005+funakoshi-dev@users.noreply.github.com>
2022-02-07 17:35:10 +0900

    Add native text and image (not interactive)

--- SwiftUI_Example/NativeView.swift
+++ SwiftUI_Example/NativeView.swift
@@ -6,10 +6,66 @@
 //
 
 import SwiftUI
+import NendAd
 
 struct NativeView: View {
+    
+    @ObservedObject var viewModel: ExternalModel = ExternalModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Group{
+                Spacer()
+                Text(self.viewModel.prText)
+                    .frame(maxWidth: .infinity,maxHeight:50,alignment: .leading)
+                Text(self.viewModel.shortText)
+                    .frame(maxWidth: .infinity,maxHeight:50,alignment: .leading)
+                Text(self.viewModel.longText)
+                    .frame(maxWidth: .infinity,maxHeight:50,alignment: .leading)
+                Image(uiImage: self.viewModel.adImage)
+                Spacer()
+                //Showボタン
+                Button(action: {
+                    self.viewModel.LoadAd()
+                }) {
+                    Text("Show")
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+class ExternalModel: ObservableObject{
+    @Published var prText:String = ""
+    @Published var shortText:String = ""
+    @Published var longText:String = ""
+    @Published var adImage:UIImage = UIImage()
+    let nativeDelegate = NativeDelegate()
+    let client = NADNativeClient(spotID: 485504, apiKey: "30fda4b3386e793a14b27bedb4dcd29f03d638e5")
+    
+    func LoadAd(){
+        self.client!.load() { (ad, error) in
+            if let nativeAd = ad {
+                // success
+                nativeAd.delegate = self.nativeDelegate
+                self.prText = "PR : " + nativeAd.prText(for: .PR)
+                self.shortText = "Short : " + nativeAd.shortText
+                self.longText = "Long : " + nativeAd.longText
+                nativeAd.loadAdImage { image in
+                    self.adImage = image!
+                }
+            } else {
+                // failure
+            }
+        }
+    }
+}
+
+class NativeDelegate: NSObject, NADNativeDelegate{
+    // 広告がクリックされた時に呼び出されます
+    func nadNativeDidClickAd(_ ad: NADNative!) {
+        print("Click ad.")
     }
 }
 
